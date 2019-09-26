c599f01865b05bf101fff043ee9c55631b208b8d
Victor Oliveira <warvimo@gmail.com>
2019-09-26 13:41:35 -0300

    add port

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ app.get('/about', (req, res  ) => {
     })
 })
 
-app.listen('3000', () =>  {
-    console.log('Listening on 3000')
+const port = process.env.PORT || '3000'
+
+app.listen(port, () =>  {
+    console.log(`Listening on ${port}`)
 })
